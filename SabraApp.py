f039ad5baccd4b0a2f3338016b3f4566116f0a71
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-16 12:39:14 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -155,7 +155,7 @@ if uploaded_file:
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('googlecloud', type=FilesConnection)
 df = conn.read("sabra-healthcare/test.csv", input_format="csv", ttl=600)
-
+st.write(df)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
