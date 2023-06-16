3d27aa22fade3d835fd2294cd8d993b4be9f824c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-16 14:20:34 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -7,7 +7,7 @@ from st_files_connection import FilesConnection
 
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.experimental_connection('googlecloud', type=FilesConnection)
+conn = st.experimental_connection('gcs', type=FilesConnection)
 df = conn.read("sabra-healthcare/test.csv", input_format="csv", ttl=600)
 
 # Print results.
