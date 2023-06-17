13c6aabee18a7ef1b1c07f8b7da305b8cc405f25
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-17 14:36:40 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -146,12 +146,15 @@ if uploaded_file:
     #df = pd.read_excel(uploaded_file)
     save_uploadedfile(uploaded_file,"")
 
-#conn = st.experimental_connection('gcs', type=FilesConnection)
-#df = conn.read("sabra-healthcare/test.csv", input_format="csv", ttl=600)
+
+conn = st.experimental_connection('gcs', type=FilesConnection)
+st.write(conn.fs.ls("sabra-healthcare/"))
+df = conn.read(""gcs://sabra-healthcare/test.csv", input_format="csv", ttl=600)
+
 
 # Print results.
-#for row in df.itertuples():
-#    st.write(f"{row.Owner} has a :{row.Pet}:")
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
 
 st.write( "By default, this P&L is for 2023 May reporting. ")
 st.write("[Learn More >](https://sabrahealthcare.sharepoint.com/)")
@@ -160,35 +163,3 @@ st.write("[Learn More >](https://sabrahealthcare.sharepoint.com/)")
 #    main(template_path_filename,finical_path_filename)
 
 
-import pyodbc
-
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from mbrentity")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
