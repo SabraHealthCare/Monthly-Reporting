fd1b8bf03f4202fb85e313e06ca152633aabee60
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-17 20:00:40 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -1,26 +1,4 @@
 import pandas as pd
-from pandas import ExcelWriter
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
-import statistics
-import os
-import xlwt
-from xlwt.Workbook import *
-import xlsxwriter
-import matplotlib.ticker as mtick
-import pyodbc
-import numpy as np
-from calendar import monthrange
-import sys
-from datetime import datetime, timedelta
-from datetime import date
-#from itertools import compress
-from os import walk
-#from openpyxl import load_workbook
-import openpyxl
-import xlrd
-import warnings
 import streamlit as st
 from st_files_connection import FilesConnection
 
@@ -31,12 +9,6 @@ df = conn.read("sabramapping/test.csv", input_format="csv", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
-
-
-
-
-
-
 
 
 
