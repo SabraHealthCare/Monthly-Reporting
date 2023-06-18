cbcce8a79bcdc9534cb94f6be26ecc9a736ed293
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-17 20:07:48 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -6,7 +6,7 @@ from st_files_connection import FilesConnection
 
 conn = st.experimental_connection('s3', type=FilesConnection)
 df = conn.read("sabramapping/test.csv", input_format="csv", ttl=600)
-
+st.write(df)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.Owner} has a :{row.Pet}:")
