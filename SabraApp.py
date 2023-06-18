8d3c1be85b2f8ea56a3c4ed54674bf9014bfdcfd
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-17 19:52:16 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -25,32 +25,12 @@ import streamlit as st
 from st_files_connection import FilesConnection
 
 
-from google.oauth2 import service_account
-from gsheetsdb import connect
-
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+conn = st.experimental_connection('s3', type=FilesConnection)
+df = conn.read("sabramapping/test.csv", input_format="csv", ttl=600)
 
 # Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
 
 
 
