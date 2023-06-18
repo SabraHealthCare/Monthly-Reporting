f0e6ef8e0b6b011e0b3c4a0f24a3592be954a2e9
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-17 20:05:53 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -3,6 +3,14 @@ import streamlit as st
 from st_files_connection import FilesConnection
 
 
+
+
+
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
+
 conn = st.experimental_connection('s3', type=FilesConnection)
 df = conn.read("sabramapping/test.csv", input_format="csv", ttl=600)
 
