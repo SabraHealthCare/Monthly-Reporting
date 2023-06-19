c78f23191160cdcb655f370baff2f46474716cf1
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-19 11:08:44 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -1,7 +1,142 @@
+
 import pandas as pd
+from pandas import ExcelWriter
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+import os
+import xlwt
+from xlwt.Workbook import *
+import xlsxwriter
+import matplotlib.ticker as mtick
+import pyodbc
+import numpy as np
+from calendar import monthrange
+import sys
+from datetime import datetime, timedelta
+from datetime import date
+#from itertools import compress
+from os import walk
+#from openpyxl import load_workbook
+import openpyxl
+import xlrd
+import warnings
 import streamlit as st
 from st_files_connection import FilesConnection
 
+
+
+
+st.title("Sabra HealthCare Reporting App")
+st.subheader("Operator name:")
+operator= st.selectbox(
+    ' ',
+    ("Advanced Recovery Systems",
+"Affinity",
+"Andrew Residence",
+"Atrium Health",
+"Aurora",
+"Avalon",
+"Avalon Realty",
+"Avamere Family",
+"Avista",
+"Baybridge",
+"Baylor Scott & White",
+"Bear Mountain",
+"Brickyard",
+"Cadia",
+"Celebration",
+"Chai",
+"Civitas",
+"Claiborne",
+"CommuniCare",
+"Consulate",
+"Covenant Care",
+"Discovery",
+"Dwight",
+"EBS",
+"EHG",
+"Emerald",
+"EmpRes Healthcare",
+"Encore",
+"Enlivant",
+"Ensign",
+"Epic Group",
+"Focused Post Acute",
+"Forest Park",
+"Fox",
+"Fundamental",
+"Genesis",
+"Golden Living",
+"Haven",
+"Health Systems",
+"Health_Dimension",
+"Healthmark Group",
+"Holiday",
+"Ignite",
+"Inspirit Senior Living",
+"Landmark Recovery",
+"Legacy Living",
+"LeoBrownGroup",
+"LifeHouse",
+"Lifes Journey",
+"Magnolia Health Sys",
+"Maison",
+"Marlin Spring - Excelsoins",
+"Marlin Spring - Spring Living",
+"Maxwell Group",
+"Meridian",
+"Meridian Health Care",
+"National Healthcare",
+"NeuroRestorative",
+"New Dawn",
+"New Haven",
+"New Orange Hills",
+"Nexion Health Mgmt",
+"Nexus Systems",
+"NMS",
+"No Relationship",
+"North Shore",
+"Nye",
+"Oakbrook",
+"Paradigm",
+"Parkside",
+"Pathways",
+"Peregrine",
+"RCA",
+"ResCare HomeCare",
+"Retirement Living",
+"RoseCastle",
+"Saber",
+"Sacred Heart",
+"Salem Villages",
+"Senior Care Centers",
+"Shelbourne",
+"Sienna",
+"Signature Behavioral",
+"Signature Healthcare",
+"Sinceri Senior Living",
+"Solvere",
+"Southern Admin",
+"Southern Healthcare",
+"Southwest LTC",
+"Spectrum Healthcare",
+"Spring Hills",
+"Stoney River",
+"Sundara",
+"Tenet",
+"The McGuire Group",
+"Titan",
+"Trinity",
+"TRMC",
+"Tryko Partners",
+"Vibra Healthcare",
+"Vivra",
+"Wachusett Ventures",
+"Welcov Healthcare",
+"Wingate Healthcare"))
+#("/"+uploadedfile.name
 
 
 conn = st.experimental_connection('s3', type=FilesConnection)
@@ -13,7 +148,6 @@ for row in df.itertuples():
 
 
 import boto3
-import streamlit as st
 
 pdf = st.file_uploader(label="Drag the PDF file here. Limit 100MB")
 if pdf is not None:
