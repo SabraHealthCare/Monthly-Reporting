a115d3c962688e038839e4422454a9e8722c3285
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-20 14:29:04 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -135,7 +135,7 @@ operator= st.selectbox(
 
 conn = st.experimental_connection('s3', type=FilesConnection)
 df = conn.read("sabramapping/test.csv", input_format="csv", ttl=600)
-st.write(df)
+#st.write(df)
 
 
 st.subheader("Upload P&L:")
