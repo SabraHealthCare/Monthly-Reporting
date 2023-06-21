accccf456e0456ad9e631025103f6dba4fc3b434
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-21 13:23:47 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -28,6 +28,23 @@ sheet_name_account_mapping="Account_Mapping"
 sheet_name_entity_mapping="Property_Mapping"
 bucket_mapping="sabramapping"
 
+def Read_Account_Mapping():
+    #read mapping format
+    obj = s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
+    format = pd.read_excel(obj['Body'].read(), sheet_name=sheet_name_account_mapping,header=0)
+        #convert tenant_account to lower case
+    format["Tenant_account"]=strip_lower_col(format["Tenant_account"])
+    format["Sabra_second_account"]=strip_upper_col(format["Sabra_second_account"])
+    format["Sabra_account"]=strip_upper_col(format["Sabra_account"])
+        # remove nan in col Sabra_account
+    mapping=format.loc[list(map(lambda x:x==x,format["Sabra_account"])),\
+                                     ["Sabra_account","Tenant_account","Sabra_second_account"]]
+    mapping=mapping.loc[list(map(lambda x:x==x,mapping["Tenant_account"])),\
+                                     ["Sabra_account","Tenant_account","Sabra_second_account"]]
+    mapping=mapping.drop_duplicates()
+    mapping=mapping.reset_index(drop=True)
+    return mapping
+
 s3 = boto3.client('s3')
 obj = s3.get_object(Bucket=bucket_mapping, Key="Operator_list.xlsx")
 operator_list = pd.read_excel(obj['Body'].read(), sheet_name='Operator_list')
@@ -50,22 +67,7 @@ uploaded_file = st.file_uploader(" ", type={"xlsx", "xlsm"}, accept_multiple_fil
 
 
 # get account mapping 
-def Read_Account_Mapping():
-    #read mapping format
-    obj = s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
-    format = pd.read_excel(obj['Body'].read(), sheet_name=sheet_name_account_mapping,header=0)
-        #convert tenant_account to lower case
-    format["Tenant_account"]=strip_lower_col(format["Tenant_account"])
-    format["Sabra_second_account"]=strip_upper_col(format["Sabra_second_account"])
-    format["Sabra_account"]=strip_upper_col(format["Sabra_account"])
-        # remove nan in col Sabra_account
-    mapping=format.loc[list(map(lambda x:x==x,format["Sabra_account"])),\
-                                     ["Sabra_account","Tenant_account","Sabra_second_account"]]
-    mapping=mapping.loc[list(map(lambda x:x==x,mapping["Tenant_account"])),\
-                                     ["Sabra_account","Tenant_account","Sabra_second_account"]]
-    mapping=mapping.drop_duplicates()
-    mapping=mapping.reset_index(drop=True)
-    return mapping
+
     
 def Upload_file_S3(file,bucket,filename):
     s3 = boto3.client('s3')
