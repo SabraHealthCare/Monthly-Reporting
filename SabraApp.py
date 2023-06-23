754187b20adf1dfdc4328282119d383ec894158f
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-22 17:47:27 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -32,7 +32,7 @@ operator= st.selectbox(' ',(operator_list))
 
 if operator != 'select operator':
     mapping_path="Mapping/"+operator+"/"+operator+"_Mapping.xlsx"
-    mapping_file =s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
+    
 
 sheet_name_account_mapping="Account_Mapping"
 sheet_name_entity_mapping="Property_Mapping"
@@ -59,14 +59,9 @@ Uploading_month=Uploading_date.month
 #------------------------------------functions------------------------------------
 def Read_Account_Mapping():
     # read account mapping
-    #account_mapping = pd.read_excel(mapping_file['Body'].read(), sheet_name=sheet_name_account_mapping,header=0)
-    entity_mapping=pd.read_excel(mapping_file['Body'].read(),sheet_name=sheet_name_entity_mapping,header=0)
-    st.write(entity_mapping)
-    #format_table=pd.read_excel(mapping_file['Body'].read(), sheet_name=sheet_name_format,header=0)
-    
-    #entity_mapping=pd.read_excel(mapping_file['Body'].read(),sheet_name=sheet_name_entity_mapping,header=0)
-        
-    #st.write(account_mapping)
+    mapping_file =s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
+    account_mapping = pd.read_excel(mapping_file['Body'].read(), sheet_name=sheet_name_account_mapping,header=0)
+
         #convert tenant_account to lower case
     account_mapping["Tenant_account"]=strip_lower_col(account_mapping["Tenant_account"])
     account_mapping["Sabra_second_account"]=strip_upper_col(account_mapping["Sabra_second_account"])
@@ -421,6 +416,15 @@ if operator != 'select operator':
 
          # def main  
         account_mapping=Read_Account_Mapping()
+        mapping_file_entity =s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
+        entity_mapping=pd.read_excel(mapping_file_entity['Body'].read(),sheet_name=sheet_name_entity_mapping,header=0)
+        st.write(entity_mapping)
+        mapping_file_format =s3.get_object(Bucket=bucket_mapping, Key=mapping_path)
+        format_table=pd.read_excel(mapping_file_format['Body'].read(), sheet_name=sheet_name_format,header=0)
+        st.write(format_table)
+  
+        
+    #st.write(account_mapping)
         format_table=pd.read_excel(mapping_file['Body'].read(), sheet_name=sheet_name_format,header=0)
         entity_mapping=pd.read_excel(mapping_file['Body'].read(),sheet_name=sheet_name_entity_mapping,header=0)
         TENANT_ID=format_table["Tenant_ID"][0]
