03fdc976989dd1147ca00145ef48732b9245cd63
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-23 12:34:25 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -346,7 +346,7 @@ def Update_Sheet_inS3(bucket,key,sheet_name,df):
     new_worksheet = workbook.create_sheet(sheet_name)
     for r in dataframe_to_rows(df, index=False, header=True):
         new_worksheet.append(r)
-    workbook.save('s3://"+bucket+"//"+key, index=False))  
+    workbook.save("s3://"+bucket+"//"+key, index=False)
     st.write("updated to S3")
     #return Upload_file_to_S3(BytesIO(workbook),bucket,key)
     
