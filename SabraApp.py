f1c90cf9913bc5fd1f17d65f8f24159ea409142b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-29 11:23:44 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -585,10 +585,10 @@ def Manage_Mapping_Main():
     if st.button("Submit"):
         with st.expander("New mapping"):
              st.write(new_account)
-    with st.expander("View Property Mapping"):
+    with st.expander("View Sabra-"+operator+" Property Mapping"):
         st.write(entity_mapping)
         download_report(entity_mapping,operator+" Property Mapping")
-    with st.expander("View Account Mapping"):      
+    with st.expander("View Sabra-"+operator+" Accounts Mapping"):      
         st.write(account_mapping)
         download_report(account_mapping,operator+" Account Mapping")
 #----------------------------------website widges------------------------------------
