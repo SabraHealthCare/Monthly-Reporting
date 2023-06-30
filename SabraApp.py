f79240edc74d85adbf54af045d9854bad7df0b22
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-29 17:08:36 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -282,7 +282,9 @@ def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name):
                
                 d_str = ''
                 for i in range(len(PL_date_header)):
-                    d_str +=str(original[i])+"——"+ str(PL_date_header[i])+", " 
+                    if PL_date_header[i]==0:
+                        continue
+                    d_str +=str(original[i])+"——"+ str(PL_date_header[i])+", "+\n 
                 st.write(d_str)
                 return PL_date_header,month_sort_index[month_index_i]
                         
