a5f276114a428695f279d11a95e651205d44ad9b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-06-29 17:27:40 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -448,7 +448,7 @@ def Sheet_Process(sheet_name,account_mapping):
        
         # remove columns what are all zero/blank 
         PL=PL.loc[:,PL.apply(pd.Series.nunique) != 1]
-        PL=PL.loc[:, (PL!= 0).any(axis=0)]
+        PL=PL.loc[:, (PL!=0 OR PL!=None).any(axis=0)]
         #remove rows with nan tenant account
         PL=PL.loc[list(filter(lambda x:x!='nan',PL.index))]
         #  new accounts don't counted yet
