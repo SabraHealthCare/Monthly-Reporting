c0010bd06406894a94e8c7ff99fb895c21d9b335
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-03 03:17:55 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -606,6 +606,7 @@ def Upload_Main(entity_mapping,account_mapping):
                     select_month=st.selectbox("Select Year/Month",diff_BPC_PL['TIME'].unique().tolist())
                 with col2:
                     select_Sabra_account=st.selectbox("Select Sabra_account",diff_BPC_PL['Sabra_account'].unique().tolist())
+                selected_data=PL_with_detail.loc[(slice(None),select_Sabra_account),["Tenant_account",select_month]]
                 st.dataframe(selected_data)
 def Manage_Mapping_Main():
     col1,col2=st.columns(2)
