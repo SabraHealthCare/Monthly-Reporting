b657349e5066054391b0330e379dc4a65d1f217c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-02 22:27:38 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -556,8 +556,8 @@ def Diff_plot(diff_BPC_PL,PL_with_detail,Total_PL):
     select_month=st.selectbox("Select Year/Month",diff_BPC_PL['TIME'].unique().tolist())
     select_Sabra_account=st.selectbox("Select Sabra_account",diff_BPC_PL['Sabra_account'].unique().tolist())
     st.write(select_Sabra_account)
-    st.write(PL_with_detail['Sabra_account'])
-    #selected_data=PL_with_detail[PL_with_detail["Sabra_account"]==select_Sabra_account][select_month]
+    st.write(PL_with_detail)
+    #selected_data=PL_with_detail["Sabra_account"]==select_Sabra_account][select_month]
     #st.dataframe(selected_data)
     download_report(PL_with_detail.reset_index(drop=False),"Detail of dismatch")  
 def download_report(df,button_display):
