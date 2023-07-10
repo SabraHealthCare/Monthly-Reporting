eb972ad17993a9cbaa5c0172c2337f8119547227
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-10 14:47:47 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -379,15 +379,15 @@ def Manage_Account_Mapping(account_mapping,new_tenant_account_list=[]):
     st.subheader("Please complete mapping for below tenant accounts:")
     #sabra account-tenant account mapping
     children_hierarchy=[]
-    parent_hierarchy_main=[{'label': "No need to map"}]
-    parent_hierarchy_second=[{'label': "No need to map"}]
+    parent_hierarchy_main=[{'label': "No need to map","value":0}]
+    parent_hierarchy_second=[{'label': "No need to map","value":0}]
     BPCAccount = s3.get_object(Bucket=bucket_mapping, Key="Initial_info.xlsx")
     BPC_Account= pd.read_excel(BPCAccount['Body'].read(), sheet_name='BPC_Account')
     for category in BPC_Account[BPC_Account["Type"]=="Main"]["Category"].unique():
         for account in BPC_Account[BPC_Account["Category"]==category]["Sabra_Account"]:
             dic={"label":account,"value":BPC_Account[BPC_Account["Sabra_Account"]==account]["BPC_Account"].item()}
             children_hierarchy.append(dic)
-        dic={"label":category,"children":children_hierarchy}
+        dic={"label":category,"value":"category","children":children_hierarchy}
         parent_hierarchy_main.append(dic)
     
     for category in BPC_Account[BPC_Account["Type"]=="Second"]["Category"].unique():
