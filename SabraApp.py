edd24da0eac5307d971571c8ec343b5d88123d30
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-11 13:57:14 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -406,13 +406,19 @@ def Manage_Account_Mapping(account_mapping,new_tenant_account_list=[]):
             new_tenant_account_list=[st.text_input("Enter new tenant account:")]
     
     for i in range (1):#(len(new_tenant_account_list)):
+    
         col1,col2=st.columns(2)    
         with col1:
             with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i])):
                 Sabra_main_account=streamlit_tree_select.tree_select(parent_hierarchy_main)
+            with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i+1])):
+                Sabra_main_account1=streamlit_tree_select.tree_select(parent_hierarchy_main)
+                
         with col2:
             with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i])):
                 Sabra_second_account= streamlit_tree_select.tree_select(parent_hierarchy_second)
+            with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i+1])):
+                Sabra_second_account1= streamlit_tree_select.tree_select(parent_hierarchy_second)
         
         if st.button("Submit Account Mapping"):
             if len(Sabra_main_account['checked'])>1:
