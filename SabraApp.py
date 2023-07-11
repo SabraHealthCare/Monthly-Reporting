a06ce7d2d327c3d5d91ac1e9034ff7738484b465
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-11 16:00:01 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -412,11 +412,12 @@ def Manage_Account_Mapping(account_mapping,new_tenant_account_list=[]):
             with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i])):
                 Sabra_main_account=streamlit_tree_select.tree_select(parent_hierarchy_main,disabled=True)
             with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i+1])):
-                #Sabra_main_account1=streamlit_tree_select.tree_select(parent_hierarchy_main)
+                Sabra_main_account1=streamlit_tree_select.tree_select(parent_hierarchy_main)
                 
-        with col2:
-            with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i])):
-                Sabra_second_account= streamlit_tree_select.tree_select(parent_hierarchy_second)
+                
+        #with col2:
+            #with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i])):
+                #Sabra_second_account= streamlit_tree_select.tree_select(parent_hierarchy_second)
             #with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i+1])):
                 #Sabra_second_account1= streamlit_tree_select.tree_select(parent_hierarchy_second)
         
