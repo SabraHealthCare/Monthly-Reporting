c70405ac87734f897a3864dc93e88fd73d24be88
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-11 16:37:45 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -404,17 +404,16 @@ def Manage_Account_Mapping(account_mapping,new_tenant_account_list=[]):
         if new_tenant_account_list==[]:
             new_tenant_account_list=[st.text_input("Enter new tenant account:")]
     
-    for i in range (1):#(len(new_tenant_account_list)):
+    for i in range (len(new_tenant_account_list)):
         col1,col2=st.columns(2)    
         with col1:
             with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i])):
-                Sabra_main_account=streamlit_tree_select.tree_select(parent_hierarchy_main,only_leaf_checkboxes=True,key=1)
+                Sabra_main_account=streamlit_tree_select.tree_select(parent_hierarchy_main,only_leaf_checkboxes=True,key=i)
                 
-            with st.expander("Map '{}' to Sabra main account".format(new_tenant_account_list[i])):
-                Sabra_main_account=streamlit_tree_select.tree_select(parent_hierarchy_main,only_leaf_checkboxes=True,key =2)
+
         with col2:
             with st.expander("Map '{}' to Sabra Second account".format(new_tenant_account_list[i])):
-                Sabra_second_account= streamlit_tree_select.tree_select(parent_hierarchy_second,only_leaf_checkboxes=True)
+                Sabra_second_account= streamlit_tree_select.tree_select(parent_hierarchy_second,only_leaf_checkboxes=True,,key=i)
             
         
         if st.button("Submit Account Mapping"):
