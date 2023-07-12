2f0c4858054e8135c14e83210b405c90e8a8774b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-07-11 22:34:18 -0700

    Update SabraApp.py

--- SabraApp.py
+++ SabraApp.py
@@ -432,8 +432,8 @@ def Manage_Account_Mapping(account_mapping,new_tenant_account_list=[]):
             st.success("Successfully mapped '{}' to '{}'".format(new_tenant_account_list[i],Sabra_main_account))
             #insert new record into account_mapping in the bottom
             account_mapping.loc[len(account_mapping.index)]=[Sabra_main_account,new_tenant_account_list[i],Sabra_second_account]
-    
-    return account_mapping    
+    st.success("{} mapped to Sabra accounts——{}".format(",".join(new_tenant_account_list)))
+    return account_mapping      
 
 def Sheet_Process(sheet_name,account_mapping):
         PL = pd.read_excel(uploaded_file,sheet_name=sheet_name,header=None)
